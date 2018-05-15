05bd8a0bd680c94a65d290605e59430f88564513
HootOwl17 <davidscout2010@hotmail.com>
2018-05-15 15:27:38 -0600

    Ejercicio 2: Ver 4 numeros separados

--- sketch_may15a/sketch_may15a.ino
+++ sketch_may15a/sketch_may15a.ino
@@ -7,10 +7,20 @@ void setup() {
  pinMode(Sensor2, INPUT);
  Serial.begin(9600);
 }
-
+int Residuo [4] = {0, 0, 0, 0};
 void loop() {
   // put your main code here, to run repeatedly:
- int Valor1 = analogRead(Sensor1);
+ int Valor1 = 1234;
+ //int Residuo = Valor1 % 10;
+ //Valor1 = Valor / 10;
  Serial.println(Valor1);
+ for (int i = 0; i <= 3; i++) {
+ Residuo [i] = Valor1 / 10;
+ //Serial.println(Residuo[i]);
+ }
+ for (int i = 3; i >= 0; i--){
+  Serial.println(Residuo[i]);
+ }
+ //Serial.println();
  delay(1000);
 }
